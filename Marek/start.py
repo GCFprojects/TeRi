5fe9f582476d5f201b3ad4e5be43b8e3ab63ad3f
kazaname <suriiw@gmail.com>
2017-02-05 15:03:13 +0100

    Dodano zabezpieczenie przed błędem pojawiającym się w przypadku nie wybrania żadnego pliku

--- Marek/start.py
+++ Marek/start.py
@@ -1,13 +1,15 @@
-import sys, os
+import os, sys
 
 from PyQt4 import QtCore, QtGui
-from TeRi import Ui_MainWindow
+from TeRi import *
+from os.path import isfile
 
 class StartQT4(QtGui.QMainWindow):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        # Ustawienie wartosci poczatkowej dla okien browser`ow
         self.ui.path_txt_window.setText("Wskarz ścieżkę do pliku txt")
         self.ui.path_xml_window.setText("Wskarz ścieżkę do pliku xml")
         # Wlasne polaczenia slotow
@@ -16,22 +18,24 @@ class StartQT4(QtGui.QMainWindow):
 
     def file_browse_txt_file(self):
         browserTxt = QtGui.QFileDialog(self)
-        plikTxt = open(browserTxt.getOpenFileName())
-        if os.path.basename(plikTxt.name)[-4:] == '.txt' or os.path.basename(plikTxt.name)[-4:] == '.csv':
-            self.ui.path_txt_window.setText(plikTxt.name)
-        else:
-            print("Hej txt")
-        #print(os.path.basename(plikTxt.name)[-4:])
+        self.filename = browserTxt.getOpenFileName()
+        if isfile(self.filename):
+            plikTxt = open(self.filename)
+            if os.path.basename(plikTxt.name)[-4:] == '.txt' or os.path.basename(plikTxt.name)[-4:] == '.csv':
+                self.ui.path_txt_window.setText(plikTxt.name)
+            else:
+                print("Hej txt")
+            #print(os.path.basename(plikTxt.name)[-4:])
 
     def file_browse_xml_file(self):
-        browserXml =QtGui.QFileDialog(self)
-        plikXml = open(browserXml.getOpenFileName())
-        if os.path.basename(plikXml.name)[-4:] == '.xml':
-            self.ui.path_xml_window.setText(plikXml.name)
-        else:
-            print("Hej xml")
-
-
+        browserXml = QtGui.QFileDialog(self)
+        self.filename = browserXml.getOpenFileName()
+        if isfile(self.filename):
+            plikXml = open(self.filename)
+            if os.path.basename(plikXml.name)[-4:] == '.xml':
+                self.ui.path_xml_window.setText(plikXml.name)
+            else:
+                print("Hej xml")
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
