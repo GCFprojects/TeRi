38f0567cd4a3702558e29be6c38947e440c49f3f
kazaname <suriiw@gmail.com>
2017-02-09 21:50:17 +0100

    Kopia plików nic nowego nie zostało wprowadzone

--- start.py
+++ start.py
@@ -17,39 +17,42 @@ class StartQT4(QtGui.QMainWindow):
         QtCore.QObject.connect(self.ui.browse_txt_button, QtCore.SIGNAL("clicked()"), self.browse_txt_file)
         QtCore.QObject.connect(self.ui.browse_xml_button, QtCore.SIGNAL("clicked()"), self.browse_xml_file)
 
+    # Wyszukiwarka plików txt i csv
     def browse_txt_file(self):
         browserTxt = QtGui.QFileDialog(self)
-        self.filename = browserTxt.getOpenFileName()
+        self.filename = browserTxt.getOpenFileName(self, 'Open file', "C:\Python34\Workspace\GlobalLogic\GCF project\TeRi")
         if isfile(self.filename):
             plikTxt = open(self.filename)
             if os.path.basename(plikTxt.name)[-4:] == '.txt' or os.path.basename(plikTxt.name)[-4:] == '.csv':
-                self.ui.path_txt_window.setText(plikTxt.name)
+                self.ui.path_txt_window.setText(os.path.basename(plikTxt.name))
             else:
-                self.showdialog()
-            #print(os.path.basename(plikTxt.name)[-4:])
+                self.showdialog(0)
 
+    # Wyszukiwarka plików xls i xlsx
     def browse_xml_file(self):
         browserXml = QtGui.QFileDialog(self)
-        self.filename = browserXml.getOpenFileName()
+        self.filename = browserXml.getOpenFileName(self, 'Open file', "C:\Python34\Workspace\GlobalLogic\GCF project\TeRi")
         if isfile(self.filename):
             plikXml = open(self.filename)
-            if os.path.basename(plikXml.name)[-4:] == '.xml':
-                self.ui.path_xml_window.setText(plikXml.name)
+            if os.path.basename(plikXml.name)[-4:] == '.xls' or os.path.basename(plikXml.name)[-5:] == '.xlsx':
+                self.ui.path_xml_window.setText(os.path.basename(plikXml.name))
             else:
-                self.showdialog()
+                self.showdialog(1)
 
-    def showdialog(self):
+    # Popup informujący o błędnym rozszeżeniu pliku
+    def showdialog(self, button):
         msg = QMessageBox()
-        msg.setIcon(QMessageBox.Information)
+        msg.setIcon(QMessageBox.Warning)
 
-
-        msg.setText("Notification")
-        msg.setInformativeText("This is additional information")
+        msg.setText("Info:")
+        if button == 0:
+            msg.setInformativeText("Wybrałeś plik o niepoprawnym rozszeżeniu.\nWybierz plik .txt lub .scv")
+        elif button == 1:
+            msg.setInformativeText("Wybrałeś plik o niepoprawnym rozszeżeniu.\nWybierz plik .xml")
         msg.setWindowTitle("Notification")
         msg.setStandardButtons(QMessageBox.Ok)
 
         retval = msg.exec()
-        print(retval)
 
 
 if __name__ == "__main__":
