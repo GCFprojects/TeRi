976f22d147accf68ae9f533699150c9c05e844aa
Marek_P <marek.polak@globallogic.com>
2017-04-20 07:28:50 +0200

    Pobiera pliki wynikowe z CMW i tworzy plik results w której znajdują się unikatowe rekordy gotowe do wprowadzenia do excel`a

--- ct.py
+++ ct.py
@@ -1,9 +1,17 @@
 import os
-
+import datetime
+# Zmienne globalne
+dir_path = ''
 # Otwieranie pliku, czytanie tekstu linia po lini. Kazdą linię przekazuje do funkcji primFormat. Funkcja tworzy listę list zwróconych z primFormat.
-def start():
-    plikTxt = open('Test\Status.csv')
-    count = sum(1 for line in open('Test\Status.csv'))-1
+def start(pathTxtFile):
+    plikTxt = open(pathTxtFile)
+    count = sum(1 for line in open(pathTxtFile))-1
+
+    userdir = os.path.expanduser('~')
+    global dir_path
+    dir_path = userdir + '\\Desktop\\TeRI_Results\\' + str(datetime.date.today())
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
 
     tab = []
     for s in range(count):
@@ -16,41 +24,42 @@ def start():
 
     tab = deleteListsWithoutResults(tab)
     checkTC(tab)
+
+    return 'Done'
 # Formatowanie tekstu. Tworzenie listy, usówanie cudzysłowów. Funkcja zwraca listę
 def primFormat(line):
     tab = []
     line = line.split(",")
-    for b in range(len(line)):
-        string = line[b]
-        if b == (len(line)-1):
-            string = string[1:len(string) - 2]
-        else:
-            string = string[1:len(string) - 1]
-        tab.append(string)
+    for s in line:
+        s=s.strip()
+        s=s.strip("\"")
+        tab.append(s)
     return tab
 # Wpisywanie do plików
 def writeToFile(tab, param=None):
+
     if param == 'Result':
-        result = open("Test\Results.txt", "a")
+        result = open(dir_path+'\\Results.txt', "a")
         for item in tab:
             result.write('\n')
             for i in item:
-                result.write('{}, '.format(i))
+                result.write('{0}, '.format(i))
         result.close()
     elif param == 'Warning':
-        warning = open("Test\Warnings.txt", "a")
+        warning = open(dir_path+"\\Warnings.txt", "a")
         for item in tab:
             warning.write('\n')
             for i in item:
-                warning.write('{}, '.format(i))
+                warning.write('{0}, '.format(i))
+        warning.close()
     elif param == 'Test':
-        result = open("Test\Results.txt", "w+")
+        result = open(dir_path+"\\Results.txt", "w+")
         for item in tab:
-            result.write('{}, '.format(item))
+            result.write('{0}, '.format(item))
         result.close()
-        warning = open("Test\Warnings.txt", "w+")
+        warning = open(dir_path+"\\Warnings.txt", "w+")
         for item in tab:
-            warning.write('{}, '.format(item))
+            warning.write('{0}, '.format(item))
         warning.close()
 # Porównywanie TC. Tworzenie listy list posiadających te same TC. Przekazywanie listy tcList do funkcji checkTcList
 def checkTC(tab):
@@ -99,5 +108,3 @@ def checkParameterAndResult(tcList):
             else:
                 writeToFile(tab, 'Warning')
     writeToFile(uniqueLists, 'Result')
-
-start()
