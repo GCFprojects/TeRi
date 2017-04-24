7c62908db3f9490174d38bde7594e1551e7e8664
Marek_P <marek.polak@globallogic.com>
2017-04-24 11:46:06 +0200

    Wprowadzono gruntowne zmiany. Pliki zapisywane są w 3 wersjach dla Raseults Fails i Warnings. Pliki zapisywane są z datami i w folderze na pulpicie. Usuwanie niechcianych wyników i inne

--- ct.py
+++ ct.py
@@ -1,10 +1,13 @@
 import os
 import datetime
+#import ImportResultsToExcel as IRTE
+
 # Zmienne globalne
 dir_path = ''
+timeNow = ''
 # Otwieranie pliku, czytanie tekstu linia po lini. Kazdą linię przekazuje do funkcji primFormat. Funkcja tworzy listę list zwróconych z primFormat.
 def start(pathTxtFile):
-    plikTxt = open(pathTxtFile)
+    plikTxt = open(pathTxtFile, encoding='utf-8')
     count = sum(1 for line in open(pathTxtFile))-1
 
     userdir = os.path.expanduser('~')
@@ -13,6 +16,8 @@ def start(pathTxtFile):
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
 
+    global timeNow
+    timeNow = datetime.datetime.now().strftime('%H_%M_%S')
     tab = []
     for s in range(count):
         line = plikTxt.readline()
@@ -23,6 +28,7 @@ def start(pathTxtFile):
         tab.remove(tab[0])
 
     tab = deleteListsWithoutResults(tab)
+    tab = deleteWarningsResults(tab)
     checkTC(tab)
 
     return 'Done'
@@ -30,37 +36,48 @@ def start(pathTxtFile):
 def primFormat(line):
     tab = []
     line = line.split(",")
+
     for s in line:
-        s=s.strip()
-        s=s.strip("\"")
+        s = s.strip()
+        s = s.strip("\"")
         tab.append(s)
     return tab
 # Wpisywanie do plików
 def writeToFile(tab, param=None):
-
     if param == 'Result':
-        result = open(dir_path+'\\Results.txt', "a")
-        for item in tab:
-            result.write('\n')
-            for i in item:
-                result.write('{0}, '.format(i))
-        result.close()
-    elif param == 'Warning':
-        warning = open(dir_path+"\\Warnings.txt", "a")
-        for item in tab:
-            warning.write('\n')
-            for i in item:
-                warning.write('{0}, '.format(i))
-        warning.close()
-    elif param == 'Test':
-        result = open(dir_path+"\\Results.txt", "w+")
+        result = open(dir_path+'\\Results_'+timeNow+'.txt', 'a', encoding='utf-8')
         for item in tab:
             result.write('{0}, '.format(item))
+        result.write('\n')
         result.close()
-        warning = open(dir_path+"\\Warnings.txt", "w+")
+    elif param == 'Warning':
+        warning = open(dir_path+"\\Warnings_"+timeNow+'.txt', 'a', encoding='utf-8')
         for item in tab:
             warning.write('{0}, '.format(item))
+        warning.write('\n')
         warning.close()
+    elif param == 'Fail':
+        fail = open(dir_path+"\\Fails_"+timeNow+'.txt', 'a', encoding='utf-8')
+        for item in tab:
+            fail.write('{0}, '.format(item))
+        fail.write('\n')
+        fail.close()
+    elif param == 'Test':
+        result = open(dir_path+"\\Results_"+timeNow+'.txt', 'w+', encoding='utf-8')
+        for item in tab:
+            result.write('{0}, '.format(item))
+        result.write('\n\n')
+        result.close()
+        warning = open(dir_path+"\\Warnings_"+timeNow+'.txt', 'w+', encoding='utf-8')
+        for item in tab:
+            warning.write('{0}, '.format(item))
+        warning.write('\n\n')
+        warning.close()
+        fail = open(dir_path + "\\Fails_" + timeNow + '.txt', 'a', encoding='utf-8')
+        for item in tab:
+            fail.write('{0}, '.format(item))
+        fail.write('\n\n')
+        fail.close()
 # Porównywanie TC. Tworzenie listy list posiadających te same TC. Przekazywanie listy tcList do funkcji checkTcList
 def checkTC(tab):
     tempList = []
@@ -87,6 +104,20 @@ def deleteListsWithoutResults(tcList):
         tcList.remove(rm)
     return tcList
 #
+def deleteWarningsResults(tab):
+    tabWarningResults = [item for item in tab if item[2] == 'INCONCLUSIVE' or item[2] == 'INVALID' or item[2] == 'ERROR']
+    for rm in tabWarningResults:
+        tab.remove(rm)
+    for item in tabWarningResults:
+        writeToFile(item, 'Warning')
+    tabFailResults = [item for item in tab if item[2] == 'FAIL']
+    for rm in tabFailResults:
+        tab.remove(rm)
+    for item in tabFailResults:
+        writeToFile(item, 'Fail')
+
+    return tab
+
 def checkParameterAndResult(tcList):
     #Deklaracje List
     uniqueLists = []
